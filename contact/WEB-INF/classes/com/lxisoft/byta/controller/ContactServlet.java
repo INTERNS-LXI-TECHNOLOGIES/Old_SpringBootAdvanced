74c41d6302298f251f106735e9b106827929e922
AnjaliLxi <anjali.chandran@lxisoft.com>
2018-06-27 22:46:06 +0530

    logging.properties

--- contact/WEB-INF/classes/com/lxisoft/byta/controller/ContactServlet.java
+++ contact/WEB-INF/classes/com/lxisoft/byta/controller/ContactServlet.java
@@ -9,14 +9,29 @@ import java.sql.*;
 import java.io.*;
 import com.lxisoft.byta.service.*;
 import com.lxisoft.byta.model.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
 
 
 public class ContactServlet extends HttpServlet {
- 
-
+	
+	static Logger LOGGER=Logger.getLogger(ContactServlet.class.getName());
+	
+	static{
+		try{
+			LogManager.getLogManager().readConfiguration(new FileInputStream("E:/lxi softwares/apache-tomcat-7.0.72/webapps/contact/contact.properties"));
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//Long phoneNumber=Long.parseLong(phone);
+		LOGGER.info("into post method");
 		
 		ContactService contactService=new ContactService();
 		
@@ -25,7 +40,6 @@ public class ContactServlet extends HttpServlet {
 		Long phoneNumber=Long.parseLong(phone);
 		String address=request.getParameter("address");
 		String email=request.getParameter("email");
-		
 		
 		ContactDto contactDto=new ContactDto(name,phoneNumber,address,email);
 		
@@ -37,12 +51,21 @@ public class ContactServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
 		
+		LOGGER.info("into get method");
+		
 		String searchName=request.getParameter("searchbyname");
 		ContactService contactService=new ContactService();
 		ContactDto contactDto=contactService.findContactByName(searchName);
 		
+		/*LOGGER.setLevel(Level.INFO);
+		
+		FileHandler fh=new FileHandler("E:/contactlog.txt");
+		LOGGER.addHandler(fh);
+		
+		LOGGER.info("sample info");
+		LOGGER.warning("warning info");*/
 
-			request.setAttribute("contactDto",contactDto);	
+		request.setAttribute("contactDto",contactDto);	
 		
 		RequestDispatcher rd=request.getRequestDispatcher("/viewdetails.jsp");
 			rd.forward(request,response);
--- contact/WEB-INF/classes/com/lxisoft/byta/service/ContactService.java
+++ contact/WEB-INF/classes/com/lxisoft/byta/service/ContactService.java
@@ -1,10 +1,35 @@
 package com.lxisoft.byta.service;
 import com.lxisoft.byta.repository.*;
 import com.lxisoft.byta.model.*;
+import java.util.*;
+import java.io.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
 
 public class ContactService{
 	
+	static Logger LOGGER=Logger.getLogger(ContactService.class.getName());
+	
+	static{
+		
+		try{
+			LogManager.getLogManager().readConfiguration(new FileInputStream("E:/lxi softwares/apache-tomcat-7.0.72/webapps/contact/contact.properties"));
+			
+		}catch(Exception ex){
+			
+			ex.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 	public void save(ContactDto contactDto){
+		
+			LOGGER.fine("into save in service");
 		
 		Contact contact=new Contact(contactDto.getName(),contactDto.getPhoneNumber(),contactDto.getAddress(),contactDto.getEmail());
 		
@@ -14,6 +39,8 @@ public class ContactService{
 	}
 	
 	public ContactDto findContactByName(String name){
+		
+		LOGGER.warning("into findContactByName in service");
 		
 		Contact contact=new ContactRepository().findContactByName(name);
 		
