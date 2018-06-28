4b27d7f918e2db57a2ab5775f14dd977b027f990
Sarathkrishna S <sarathkrishna.s@lxisoft.com>
2018-06-28 20:39:43 +0530

    logging completd

--- DataSourceSample/WEB-INF/classes/DataSourceServlet.java
+++ DataSourceSample/WEB-INF/classes/DataSourceServlet.java
@@ -4,27 +4,31 @@ import java.io.*;
 import javax.sql.*;
 import javax.naming.*;
 import java.sql.*;
+import java.util.logging.*;
+
 public class DataSourceServlet extends HttpServlet
 {
+	static Logger LOGGER=Logger.getLogger(DataSourceServlet.class.getName());
+	
 	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException
 	{
-		try
-		{
+		try{
+			LogManager.getLogManager().readConfiguration(new FileInputStream("C:/Users/Basil jude/Desktop/apache-tomcat-7.0.72/webapps/DataSourceSample/WEB-INF/classes/logging.properties"));
 		DataSource ds = null;
 		Connection conn = null;
 		Statement stmt = null;
 		response.setContentType("text/html");
 		String name = request.getParameter("name");
-		System.out.println(name);
+		LOGGER.info("Obtain our environment naming context");
 		// Obtain our environment naming context
 		Context initCtx = new InitialContext();
 		ds = (DataSource) initCtx.lookup("java:comp/env/jdbc/datasource");
 		/* Context envCtx = (Context) initCtx.lookup("java:comp/env"); */
 		// Look up our data source
+LOGGER.info("Allocate and use a connection from the pool");
 		// Allocate and use a connection from the pool
 		conn = ds.getConnection();
 		stmt = conn.createStatement();
-		System.out.println("Created connection to database.");
 		String sql = "INSERT INTO save VALUES ('"+name+"')";
 		stmt.executeUpdate(sql);
 		conn.close();
