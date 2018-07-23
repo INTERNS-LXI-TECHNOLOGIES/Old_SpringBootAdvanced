bf12d2b1f1bb4965a99128ecf88ea3881dbfcfe2
Dheerajdas1992 <dheeraj.das@lxisoft.com>
2018-07-23 15:16:02 +0530

    LI-135

--- FacebookApp1/src/main/java/com/lxisoft/fbapp/model/Prison.java
+++ FacebookApp1/src/main/java/com/lxisoft/fbapp/model/Prison.java
@@ -1,21 +1,61 @@
-package com.lxisoft.fbapp.model;
+package com.lxisoft.model;
 
 /**
 * This class is for control prison
-
+*
 * @author Dheeraj das
+*
 * @version 1.0
-
 */
 public class Prison
 {
 	/**
-	*This is a prisoner object
+	*This is a reference to prisoner 
 	*/
-	Prisoner prisoner;
+	private Prisoner prisoner;
 	
 	/**
-	*This is a hall object
-	*/	
-	Hall hall;
+	*This is a reference to hall
+	*/
+	private Hall hall;
+	
+	/**
+	* This is a setter which sets the prisoner
+	*
+	*@param prisoner - the prisoner to  set
+	*/
+	public void setPrisoner(Prisoner prisoner)
+	{
+		this.prisoner = prisoner;
+	}
+	
+	/**
+	*This is a getter which returns the prisoner
+	*
+	*@return prisoner - the prisoner to get
+	*/
+	public Prisoner getPrisoner()
+	{
+		return prisoner;
+	}
+	
+	/**
+	* This is a setter which sets the hall
+	*
+	*@param hall - the hall to  set
+	*/
+	public void sethall(Hall hall)
+	{
+		this.hall = hall;
+	}
+	
+	/**
+	*This is a getter which returns the hall
+	*
+	*@return hall - the hall to get
+	*/
+	public Hall getHall()
+	{
+		return hall;
+	}
 }
