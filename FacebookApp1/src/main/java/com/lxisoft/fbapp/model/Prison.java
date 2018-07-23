27ca0aa4be130839dd9d1fc0acab43dd03af97ce
Dheerajdas1992 <dheeraj.das@lxisoft.com>
2018-07-23 11:31:33 +0530

    Model class of hall,prison,security question

---
+++ FacebookApp1/src/main/java/com/lxisoft/fbapp/model/Prison.java
@@ -0,0 +1,21 @@
+package com.lxisoft.fbapp.model;
+
+/**
+* This class is for control prison
+
+* @author Dheeraj das
+* @version 1.0
+
+*/
+public class Prison
+{
+	/**
+	*This is a prisoner object
+	*/
+	Prisoner prisoner;
+	
+	/**
+	*This is a hall object
+	*/	
+	Hall hall;
+}
